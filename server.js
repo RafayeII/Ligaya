8173957572fc586a2e82ca0eb9841ae12994a0b0
RafayeII <113429583+RafayeII@users.noreply.github.com>
2022-12-08 16:14:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASS,
-  database : process.env.DB_NAME,
+  database : process.env.DB_NAME
 });
 
 //CREATES SESSION IN DATABASE
