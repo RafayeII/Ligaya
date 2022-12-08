050bf1358e70293bfab4a1c23696dd1fd598b7f3
RafayeII <113429583+RafayeII@users.noreply.github.com>
2022-12-08 13:13:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -390,5 +390,4 @@ app.get('/tagaytay', (req, res) => {
     res.redirect('login');
 });
 
-//PORT IS 3000
-app.listen(3000, () => console.log('listening on port 3000!'));
+app.listen(8080, () => console.log('listening on port 3000!'));
