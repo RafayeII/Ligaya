46241ee1ad8f72dd72606ed0a1a92b5c3773c27e
RafayeII <113429583+RafayeII@users.noreply.github.com>
2022-12-09 00:09:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser')
-const mysql = require('mysql');
+const {createPool} = require('mysql');
 const MySQLStore = require('express-mysql-session')(session);
 const bcrypt = require('bcrypt');
 
 const app = express();
 
 //CONNECTS TO THE LIGAYA.SQL DATABASE USING WORKBENCH
-const db = mysql.createConnection({
+const db = new createPool({
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASS,//CHANGE ACCORDING TO YOUR WORKBENCH PASSWORD
   database : process.env.DB_NAME,
-  
+  queryTimeout : 60000
 });
 
 //CREATES SESSION IN DATABASE
 const sessionStore = new MySQLStore({
-  expiration: 10800000,
+  expiration: 60000,
   createDatabaseTable: true,
   schema: {
     tableName: 'session',
@@ -30,17 +30,6 @@ const sessionStore = new MySQLStore({
     }
   }
 }, db);
-
-//CONFIRMS CONNECTION OR THROWS ERROR
-db.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    db.disconnect();
-    return;
-  }
-  else
-    console.log('connected as ID' + db.threadId);
-});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
