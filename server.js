627d7cab3f2c36dbd89c7727d8cd7587c0cf000c
RafayeII <113429583+RafayeII@users.noreply.github.com>
2022-12-08 21:01:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const app = express();
 
 //CONNECTS TO THE LIGAYA.SQL DATABASE USING WORKBENCH
 const db = mysql.createConnection({
-  connectionLimit : 1000,
-  connectTimeout  : 60 * 60 * 1000,
-  acquireTimeout  : 60 * 60 * 1000,
-  timeout         : 60 * 60 * 1000,
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASS,
